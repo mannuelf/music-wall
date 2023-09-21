1d8d148bdaa91385d4206013d4fa3000713d207e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-09-21 20:53:00 +0200

    turns prerender off and edge func on

--- src/routes/[username]/+page.server.ts
+++ src/routes/[username]/+page.server.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 import LastFmApi from 'lastfm-nodejs-client';
 import type { TopArtistsResponse, UserResponse } from 'lastfm-nodejs-client/@types';
-export const prerender = true;
+export const prerender = false;
 
 export const load = async ({ params }) => {
 	const lastFm = LastFmApi();
