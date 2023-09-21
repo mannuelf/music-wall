b76413367c4e4aff30a053c8733d9793e9d2a739
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-09-21 20:47:39 +0200

    prerender true

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-export const prerender = false;
+export const prerender = true;
 
 export const load = async () => {
 	return {
--- src/routes/[username]/+page.server.ts
+++ src/routes/[username]/+page.server.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 import LastFmApi from 'lastfm-nodejs-client';
 import type { TopArtistsResponse, UserResponse } from 'lastfm-nodejs-client/@types';
-export const prerender = false;
+export const prerender = true;
 
 export const load = async ({ params }) => {
 	const lastFm = LastFmApi();
