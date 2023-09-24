0cc2592a5a53164e3a5c53053b172cbaf912ddbd
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-09-24 23:33:10 +0200

    incude node modules in build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit(),purgeCss()],
+	plugins: [sveltekit(), purgeCss()],
+	build: {
+		commonjsOptions: {
+			include: [/node_modules/]
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
